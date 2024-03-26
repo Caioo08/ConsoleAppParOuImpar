b4f1adf5894d8fd259e6954c7264149e58c3892e
Usuario <Usuario@CPD02-M02>
2024-03-26 11:01:18 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleAppParOuImpar/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppParOuImpar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Escolha Pares ou impares[0-par, 1- impar]");
+            int escol = int.Parse(Console.ReadLine());
+            int cont = escol;
+            Console.Clear();
+            while (cont <= 20) {
+                
+                Console.WriteLine(cont);
+                cont += 2;
+            }
+            Console.ReadKey();
+        }
+    }
+}
